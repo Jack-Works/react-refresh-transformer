955a88cd7b043654372be6b8ee9164d78e0ef733
Jack Works <jackworks@protonmail.com>
2021-01-16 21:51:38 +0800

    chore: make commonjs import more convenient

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -2,10 +2,17 @@
 import core from './core'
 import typescript from 'typescript'
 import crypto from 'crypto'
-export default (function (option = {}) {
+const nodeWrapper: typeof core = function (option = {}) {
     return core({
         ts: typescript,
         hashSignature: (signature) => crypto.createHash('sha1').update(signature).digest('base64'),
         ...option,
     })
-} as typeof core)
+}
+
+// Our build tool chain doesn't allow use to write export = nodeWrapper
+Object.assign(nodeWrapper, { default: nodeWrapper })
+module.exports = nodeWrapper
+module.exports.__esModule = true
+// Give it a correct typing
+export default nodeWrapper
